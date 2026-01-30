c312da400841d65d6a1cfe65a18bb52ac924f4a4
aly elsayed <ealy5550@gmail.com>
2026-01-31 00:00:08 +0200

    update profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_app_new/features/profile/presentation/screens/favorite_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MyFavorite();
   }
 }
